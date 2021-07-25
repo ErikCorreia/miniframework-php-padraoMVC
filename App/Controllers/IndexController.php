6a51540f622776265b47e04de846cee2bb1626e8
Erik Correia <erickcorreia727@gmail.com>
2021-07-25 03:02:49 -0400

    ++

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,20 +2,16 @@
 
     namespace App\Controllers;
 
-    class IndexController {
+    use MF\Controller\Action;
+
+    class IndexController extends Action{
+
         public function index(){
-            $dados = array('Tv', 'sofá', 'Cama');
-            $this->render('index', $dados);
+            $this->view->dados = array('Tv', 'sofá', 'Cama');
+            $this->render('index');
         }
         public function sobreNos(){
             $this->render('sobreNos');
-        }
-
-        public function render($view, $dados){
-            $classeAtual = get_Class($this);
-            str_replace('App\\Controllers\\', '', $classeAtual);
-            echo $classeAtual;
-            require_once "../App/Views/index/".$view.".phtml";
         }
     }
 
