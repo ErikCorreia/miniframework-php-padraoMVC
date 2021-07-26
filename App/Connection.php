2e6822cd698746e8fba7ecfb42bc7cc7e13767c4
Erik Correia <erickcorreia727@gmail.com>
2021-07-26 02:48:17 -0400

    Adicionado a camada model e o metodo de conexão com banco de dados

--- App/Connection.php
+++ App/Connection.php
@@ -1,9 +1,24 @@
-<?pho
+<?php
 
    namespace App;
-
+//Função para fazer a conexão com o banco de dados
    Class Connection {
-      //Code...
+      public static function getDb() {
+         try {
+            //a variavel responsavel pela conexão deve ser alteradas de acordo com o banco de dados da aplicação, alterando o "host" e "dbname"
+            $Connect = new \PDO(
+               "mysql:host=localhost; dbname=mvc; charset=utf8",
+               //usuario
+               "root",
+               //senha
+               ""
+            );
+            return $Connect;
+
+         } catch (\PDOException $e) {
+            //fazer tratativas de erros 
+         }
+      }
    }
 
 ?>
--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -3,13 +3,24 @@
     namespace App\Controllers;
 
     use MF\Controller\Action;
+    use App\connection;
+    use App\Models\Produto;
 
     class IndexController extends Action{
 
         public function index(){
-            $this->view->dados = array('Tv', 'sofá', 'Cama');
+            //instância de conexão
+            //Chama o metodo getDb da classe connection definida como static em "App\connection", que faz a conexão com o banco de dados
+            $connect = Connection::getDb();
+
+           //instancia modelo com o parametro que fez a conexão
+            $produto = new Produto($connect);
+            $produtos = $produto->getProdutos();
+            $this->view->dados = $produtos;
+
             $this->render('index');
         }
+
         public function sobreNos(){
             $this->render('sobreNos');
         }
